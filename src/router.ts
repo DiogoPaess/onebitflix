0be52fdd14c4bf890941175abe731050e7aa5ac5
Diogo Paes <diogo.1@hotmail.com>
2024-03-12 16:48:55 +0000

    Getting course information

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { categoriesController } from "./controllers/categoriescontrollers";
+import { coursesController } from "./controllers/coursescontroller";
 
 const router = express.Router();
 
 router.get("/categories", categoriesController.index);
 router.get("./category/:id", categoriesController.show);
+
+router.get("./courses/:id", coursesController.show);
 
 export { router };
