e747268b848b594e239849b8df8c7b2b7f61d19b
Diogo Paes <diogo.1@hotmail.com>
2024-03-17 11:59:52 +0000

    Getting your favorite courses

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -1,6 +1,25 @@
 import { Favorite } from "../models";
 
 export const favoriteService = {
+  findByUserId: async (userId: number) => {
+    const favorites = await Favorite.findAll({
+      attributes: [["userId", "userId"]],
+      where: { userId },
+      include: {
+        association: "Course",
+        attributes: [
+          "id",
+          "name",
+          "synopses",
+          ["thumbnail_url", "thumnailUrl"],
+        ],
+      },
+    });
+    return {
+      userId,
+      courses: favorites.map((favorite) => favorite.Course),
+    };
+  },
   create: async (userId: number, courseId: number) => {
     const favorite = await Favorite.create({
       userId,
